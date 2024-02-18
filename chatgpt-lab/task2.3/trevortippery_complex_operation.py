ea781f73b500659a6a5f20a82ef15ab94cd71f8c
Trevor <trevortippery221@gmail.com>
2024-02-17 17:38:22 -0800

    added initial code snippet for task2.3

---
+++ chatgpt-lab/task2.3/trevortippery_complex_operation.py
@@ -0,0 +1,11 @@
+def complex_operation(number):
+    """
+    Perform a series of mathematical operations on the input number.
+
+    Parameters:
+    - number (float or int): The input number on which the operations are performed.
+
+    Returns:
+    float or int: The result of the complex mathematical operations.
+    """
+    return (((number * 5) / 6) + number - 6) * 6
